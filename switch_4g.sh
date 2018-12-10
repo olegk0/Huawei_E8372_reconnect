1a9fb428fd5e730006ac70b42e7676c70a94780d
olegk0 <olegvedi@gmail.com>
2018-12-10 11:38:02 +0300

    Update switch_4g.sh
    reworked + add get statistic before reset  to /tmp/4g_stat.txt

--- switch_4g.sh
+++ switch_4g.sh
@@ -5,39 +5,22 @@ USER="login" # admin
 PASSWORD="password"
 MODEM_IP="192.168.8.1"
 
-curl -i -s -X GET "http://$MODEM_IP/html/home.html" > /tmp/ses_tok.txt
-COOKIE=`grep "SessionID=" /tmp/ses_tok.txt | cut -d ':' -f2 | cut -d ';' -f1`
-TOKEN=`grep -m2 "csrf_token" /tmp/ses_tok.txt | tail -n1 | cut -d '"' -f4`
-
-#grep -m2 "csrf_token" /tmp/ses_tok.txt | tail -n1 | cut -d '"' -f4  > SecondToken.txt
-#nodejs cli.js
-
-#USER=`cat User.txt`
-#PASSWORD=`cat Password.txt`
+ses_tok=`curl -s -i -X GET "http://$MODEM_IP/html/home.html"`
+ses_tok=`echo "$ses_tok" | tr '\r' '\n'`
+COOKIE=`echo "$ses_tok" | grep "SessionID=" | cut -d ':' -f2 | cut -d ';' -f1`
+TOKEN=`echo "$ses_tok" | grep -m2 "csrf_token" | tail -n1 | cut -d '"' -f4`
 
 #base64encode(SHA256(user + base64encode(SHA256(pass)) + token));
-psd=`printf "$PASSWORD" | sha256sum | cut -d " " -f1`
-#echo "bash0:$psd|"
-psd=`printf "${psd}" | base64 -w0`
-#psd="${psd:0:-2}=="
-#echo "bash1:$psd|"
-psd="${USER}${psd}${TOKEN}"
-#echo "bash2:$psd|"
-psd=`printf "$psd" | sha256sum | cut -d " " -f1`
-#echo "bash3:$psd|"
-psd=`printf "$psd" | base64 -w0`
-
-#echo "bash:${psd}|"
-#PASSWORD=`cat encPassword.txt`
-#echo "js:${PASSWORD}|"
-PASSWORD=$psd
-
+PASSWORD=`printf "$PASSWORD" | sha256sum | cut -d " " -f1`
+PASSWORD=`printf "${PASSWORD}" | base64 -w0`
+PASSWORD="${USER}${PASSWORD}${TOKEN}"
+PASSWORD=`printf "$PASSWORD" | sha256sum | cut -d " " -f1`
+PASSWORD=`printf "$PASSWORD" | base64 -w0`
 
 LOGIN_REQ="<?xml version "1.0" encoding="UTF-8"?><request><Username>$USER</Username><Password>$PASSWORD</Password><password_type>4</password_type></request>"
-#echo "$LOGIN_REQ" > Request.xml
-#curl -X POST -# -v -d @Request.xml "http://$MODEM_IP/api/user/login" \
+
 echo "##############################################################################################################"
-curl -X POST -# -v -d "$LOGIN_REQ" "http://$MODEM_IP/api/user/login" \
+login_resp_hdr=`curl -s -i -X POST -d "$LOGIN_REQ" "http://$MODEM_IP/api/user/login" \
 -H "__RequestVerificationToken: $TOKEN" \
 -H "Content-Type: application/x-www-form-urlencoded" \
 -H "Cookie: $COOKIE" \
@@ -45,22 +28,32 @@ curl -X POST -# -v -d "$LOGIN_REQ" "http://$MODEM_IP/api/user/login" \
 -H "Accept-Encoding: gzip, deflate" \
 -H "Pragma: no-cache" \
 -H "Connection: keep-alive" \
--H "Accept-Language: en-us" \
---dump-header /tmp/login_resp_hdr.txt
-# > /dev/null
-echo
+-H "Accept-Language: en-us"`
+
+#--dump-header /tmp/login_resp_hdr.txt  > /dev/null
 echo "##############################################################################################################"
-SESSION_ID=`grep "SessionID=" /tmp/login_resp_hdr.txt | cut -d ':' -f2 | cut -d ';' -f1`
-TOKEN=`grep "__RequestVerificationTokenone" /tmp/login_resp_hdr.txt | cut -d ':' -f2`
+login_resp_hdr=`echo "$login_resp_hdr" | tr '\r' '\n'`
+echo "##############################################################################################################"
+SESSION_ID=`echo "$login_resp_hdr" | grep "SessionID=" | cut -d ':' -f2 | cut -d ';' -f1`
+TOKEN=`echo "$login_resp_hdr" | grep "__RequestVerificationTokenone" | cut -d ':' -f2`
 
 echo "admin session_id"
 echo "$SESSION_ID"
-TOKEN="${TOKEN:0:-1}"
+#TOKEN="${TOKEN:0:-1}"
+echo "Token"
 echo "$TOKEN"
 echo
 echo "##############################################################################################################"
 
-curl -v -X POST -d "<?xml version='1.0' encoding='UTF-8'?><request><dataswitch>$on</dataswitch></request>" \
+curl -s -X GET "http://$MODEM_IP/api/monitoring/traffic-statistics" \
+-H "Cookie: $SESSION_ID" \
+-H "__RequestVerificationToken: $TOKEN" \
+-H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8"  > /tmp/4g_stat.txt
+
+echo
+echo "##############################################################################################################"
+
+curl -s -X POST -d "<?xml version='1.0' encoding='UTF-8'?><request><dataswitch>$on</dataswitch></request>" \
 "http://$MODEM_IP/api/dialup/mobile-dataswitch" \
 -H "Cookie: $SESSION_ID" \
 -H "__RequestVerificationToken: $TOKEN" \
